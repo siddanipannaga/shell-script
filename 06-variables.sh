58ce51d651f1b3230d42674192e203a8f97ea179
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-16 11:09:30 +0530

    added second script

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "enetr your username"
+echo "enter your username"
 read USERNAME # the value you enetr will e automatically attached to user
-echo :username is:$USERNAME"
+echo :username is:$1USERNAME"
