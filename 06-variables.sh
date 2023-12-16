36aa81453f8a6bd72083a58b263265ac1d28aab8
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-16 11:24:37 +0530

    added second script

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,4 +4,4 @@ echo "enter your username"
 read -s USERNAME # the value you enetr will e automatically attached to user
 echo "enter your password"
 read -s PASSWORD
-echo ""username is: $USERNAME, password is : $PASSWORD"
+echo "username is: $USERNAME, password is : $PASSWORD"
