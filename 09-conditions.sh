5f49b186425c220a3b3e9b8c950bfa6c05ec042b
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-16 17:44:33 +0530

    conditions added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
    echo "Given number $NUMBER is greater than 100"
 else
-   echo "Given number $NUMBER is not greater than 100"
+   echo "Given number $NUMBER is not greater than 100"  
 fi
